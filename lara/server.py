7d2ed3120ccbc5216bfbaebb9b4139f46427b4fb
Philipp Wiesner <mail@philippwiesner.com>
2018-01-24 14:27:46 +0100

    implemented OAuth handshake

--- lara/server.py
+++ lara/server.py
@@ -13,3 +13,37 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/login/callback')
+def login_callback():
+    #if g.get('states') is None:
+    #    g.states = []
+    code = request.args.get('code')
+    if code is None:
+        random_state = 'aaa' # ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        #g.states.append(random_state)
+        redirect_url = 'https://github.com/login/oauth/authorize?client_id={}&state={}'.format(app.config['GITHUB_OAUTH_CLIENT_ID'], random_state)
+        return redirect(redirect_url, code=302)
+    state = request.args.get('state')
+    if state in ['aaa']:
+        conn = HTTPSConnection("github.com")
+        conn.request(
+            method='POST',
+            url='/login/oauth/access_token?client_id={}&client_secret={}&code={}&state={}'.format(
+                app.config['GITHUB_OAUTH_CLIENT_ID'],
+                app.config['GITHUB_OAUTH_CLIENT_SECRET'],
+                code,
+                state
+            ),
+            headers={
+                "User-Agent": "LaraTUB/lara"
+            }
+        )
+        response = conn.getresponse()
+        response_text = response.read().decode('utf-8')
+        conn.close()
+
+        access_token = parse_qs(response_text)['access_token'][0]
+        return parse_qs(response_text)
+    else:
+        raise Exception('Bad state', status_code=400)
+
