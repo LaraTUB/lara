60e53005801308baeaad533543652c5a001edb65
Philipp Wiesner <mail@philippwiesner.com>
2018-01-31 13:40:21 +0100

    yet another try

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 PATH=lara
 ssh ec2-user@$STAGING_SERVER "mkdir -p $PATH"
-# rsync -rav -e ssh --exclude='.git/' --exclude=scripts/ --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
+# rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
