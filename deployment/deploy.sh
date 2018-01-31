44c5e967025a32da843f7440790e0ea6a699bf7d
Philipp Wiesner <mail@philippwiesner.com>
2018-01-31 14:17:30 +0100

    tesing deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,4 @@
-PATH=lara
-ssh ec2-user@$STAGING_SERVER "mkdir -p $PATH"
+#!/usr/bin/env sh
+
+ssh ec2-user@$STAGING_SERVER "mkdir -p lara"
 # rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
