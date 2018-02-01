a66d4962fae145ce2d32d9fa9108fe4c2cb6ebbd
Philipp Wiesner <mail@philippwiesner.com>
2018-02-01 12:06:28 +0100

    changed shebang

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
-
+#!/bin/sh
+echo $STAGING_SERVER
 ssh ec2-user@$STAGING_SERVER "mkdir -p lara"
 # rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
