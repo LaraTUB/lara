41c9024ec41abea646b1bc64cefecbf0462753b4
Philipp Wiesner <mail@philippwiesner.com>
2018-02-01 12:54:40 +0100

    testing rsync

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ls .
 ssh ec2-user@$STAGING_SERVER "mkdir -p lara"
-ls ./
-
-rsync -rav --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
+rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:lara
+# pip install --upgrade -r requirements.txt
