327338724e627ac9eef29e8afc13961022565188
Philipp Wiesner <mail@philippwiesner.com>
2018-02-01 12:17:57 +0100

    testing rsync

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo $STAGING_SERVER
+
 ssh ec2-user@$STAGING_SERVER "mkdir -p lara"
-# rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
+rsync -rav -e ssh --exclude='.git/' --exclude='.travis.yml' --delete-excluded ./ ec2-user@$STAGING_SERVER:$PATH
