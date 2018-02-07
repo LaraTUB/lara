66df17cfd939a1c2346710ba25e072d5e9af938e
Philipp Wiesner <mail@philippwiesner.com>
2018-02-07 01:00:27 +0100

    little bugfix in deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,8 +4,8 @@ echo ec2-user@$STAGING_SERVER
 
 rsync -rav -e ssh --exclude='.git/' \
                   --exclude='.travis.yml' \
-                  --exclude='app/app/instance/config.py' \
-                  --exclude='app/app/instance/github_key.pem' \
+                  --exclude='app/instance/config.py' \
+                  --exclude='app/instance/github_key.pem' \
                   --delete ./ ec2-user@$STAGING_SERVER:lara
 
 ssh ec2-user@$STAGING_SERVER "cd lara && \
