65eb77b17bbbbe8524222702c10fa047c0ae9c9b
Philipp Wiesner <mail@philippwiesner.com>
2018-02-21 17:32:14 +0100

    updated deployment, persisted db in docker-compose and added script to pull the db from the server

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,9 +4,9 @@ echo ec2-user@$STAGING_SERVER
 
 rsync -rav -e ssh --exclude='.git/' \
                   --exclude='instance/*' \
-                  --delete ./ ec2-user@$STAGING_SERVER:lara
+                  --delete ./ ec2-user@$STAGING_SERVER:app
 
-ssh ec2-user@$STAGING_SERVER "cd lara && \
+ssh ec2-user@$STAGING_SERVER "cd app && \
     docker-compose down && \
     docker-compose build && \
     docker-compose up -d"
---
+++ pull-database.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ]; then
+    echo "Usage: pull-database.sh <ec2-privatekey>";
+else
+    scp -i $1 ec2-user@ec2-54-89-185-46.compute-1.amazonaws.com:app/instance/lara.db ./app/instance/lara.db
+fi
