84e8579b30e988c358dcba685359b5cb28b1c84b
Philipp Wiesner <mail@philippwiesner.com>
2018-03-10 11:22:20 +0100

    removed all unnecessary features

--- app/app/webhook.py
+++ app/app/webhook.py
@@ -19,74 +19,46 @@ LOG = logging.getLogger(__name__)
 @application.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    LOG.debug("Request from dialogflow is: %s" % req)
+    LOG.debug("Request from dialogflow: %s" % req)
 
-    # TODO Refactor
-    kwargs = utils.extract_slack_parameters(req)
-    kwargs.update(utils.merge_parameters(req))
-
-    # Why do we need to do this?
-    if kwargs.get("assignee.login"):
-        kwargs['session_id'] = "{}:{}".format(kwargs.get("assignee.login"), kwargs['session_id'])
+    if "originalRequest" not in req or req["originalRequest"]["source"] != "slack":
+        return respond(speech="Only requests via Slack supported")
 
     # Authenticate
-    if "originalRequest" not in req:
-        return respond(speech="Only requests via Slack supported")
+    slack_user_id = req["originalRequest"]["data"]["event"]["user"]
+    try:
+        user = dbapi.user_get_by__slack_user_id(slack_user_id)
+    except exceptions.UserNotFoundBySlackUserId:
+        LOG.debug("Slack User unknown, asking for authentication with token xxx")
+        return respond(speech="Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
     else:
-        original_request = req["originalRequest"]
-        if original_request["source"] != "slack":
-            LOG.error("Only Slack supported")
-            raise exceptions.LaraException()
-        slack_user_id = original_request["data"]["event"]["user"]
-
-        try:
-            user = dbapi.user_get_by__slack_user_id(slack_user_id)
-        except exceptions.UserNotFoundBySlackUserId:
-            LOG.debug("Slack User unknown, asking for authentication with token xxx")
+        if not user.github_login:
+            LOG.debug("Gihub Login unknown, asking for authentication with token xxx")
             return respond(speech="Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
-        else:
-            if not user.github_login:
-                LOG.debug("Gihub Login unknown, asking for authentication with token xxx")
-                return respond(speech="Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
+
+    # Handle actions
+    action = req["result"]["action"]
+    if action == 'hello':
+        return respond(speech="Hi " + user.github_login)
 
 
-        github_login = user.github_login
-        LOG.debug("Request by " + kwargs["assignee.login"])
+    # try:
+    #     # TODO way to pass
+    #     results = handler(**kwargs)
+    # except exceptions.RepositoryNotProvidedException:
+    #     LOG.debug("Trigger Repository Missing Event.")
+    #     followup_event = trigger.repository_missing_event(action="{}_{}".format(name, action), **kwargs)
+    #     return respond(**followup_event)
+    # except exceptions.IssueCommentNotFinishedException:
+    #     LOG.debug("Trigger Issue Comemnt Not Finished Event.")
+    #     followup_event = trigger.issue_comment_not_finished_event(action="{}_{}".format(name, action), **kwargs)
+    #     return respond(**followup_event)
+    # except exceptions.IssueIdNotProvidedException:
+    #     LOG.error("Issue id not provided.")
+    # except:
+    #     raise exceptions.LaraException()
 
-    kwargs["assignee.login"] = github_login
-    try:
-        if req["result"]["action"]:
-            name, action = req["result"]["action"].split("_")
-        else:
-            name, action = kwargs.pop("action", "").split("_")
-        LOG.debug("Request for github object *%s*, play *%s* action" % (name, action))
-        LOG.debug("Request parameters are %s" % kwargs)
-        # NOTE: each user has a git object handler.
-        handler = getattr(get_git_object("{}:{}".format(github_login, name)),
-                          action)
-    except ValueError:
-        # TODO implement handler for more general commands like "ListAllIssues"
-        action = req["result"]["action"]
-        def handler():
-            return "Basic result"
-
-    try:
-        # TODO way to pass
-        results = handler(**kwargs)
-    except exceptions.RepositoryNotProvidedException:
-        LOG.debug("Trigger Repository Missing Event.")
-        followup_event = trigger.repository_missing_event(action="{}_{}".format(name, action), **kwargs)
-        return respond(**followup_event)
-    except exceptions.IssueCommentNotFinishedException:
-        LOG.debug("Trigger Issue Comemnt Not Finished Event.")
-        followup_event = trigger.issue_comment_not_finished_event(action="{}_{}".format(name, action), **kwargs)
-        return respond(**followup_event)
-    except exceptions.IssueIdNotProvidedException:
-        LOG.error("Issue id not provided.")
-    except:
-        raise exceptions.LaraException()
-
-    return respond(speech=json.dumps(results))
+    return respond(speech="Unknown action")
 
 
 def respond(**kwargs):
