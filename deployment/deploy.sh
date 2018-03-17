256592efa06b5990a4d289e8da1b957f2e33c868
Philipp Wiesner <mail@philippwiesner.com>
2018-03-17 17:40:11 +0100

    Static files are now serverd correctly

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,9 +4,9 @@ echo ec2-user@$STAGING_SERVER
 
 rsync -rav -e ssh --exclude='.git/' \
                   --exclude='instance/*' \
-                  --delete ./ ec2-user@$STAGING_SERVER:app
+                  --delete ./ ec2-user@$STAGING_SERVER:lara
 
-ssh ec2-user@$STAGING_SERVER "cd app && \
+ssh ec2-user@$STAGING_SERVER "cd lara && \
     docker-compose down && \
     docker-compose build && \
     docker-compose up -d"
