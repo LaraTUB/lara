0348809058d78af3388d29dd6d560cf318e6de06
Philipp Wiesner <mail@philippwiesner.com>
2018-03-28 00:28:09 +0200

    some cleanup, added comments and adapted pull-database.sh to new remote structure

--- app/app/webhook.py
+++ app/app/webhook.py
@@ -14,7 +14,7 @@ from app.db import api as dbapi
 LOG = logging.getLogger(__name__)
 
 
-@application.route('/webhook', methods=['GET', 'POST'])
+@application.route("/webhook", methods=["GET", "POST"])
 def webhook():
     req = request.get_json(silent=True, force=True)
     LOG.debug("Request from dialogflow: %s" % req)
@@ -28,17 +28,17 @@ def webhook():
         user = dbapi.user_get_by__slack_user_id(slack_user_id)
     except exceptions.UserNotFoundBySlackUserId:
         LOG.debug("Slack User unknown, asking for authentication with token xxx")
-        return respond(speech="Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
+        return respond(speech="Hi! Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
 
     if not user.github_token:
         LOG.debug("Gihub Login unknown, asking for authentication with token xxx")
-        return respond(speech="Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
+        return respond(speech="Hi! Please authenticate with Github:\n" + auth.build_authentication_message(slack_user_id))
 
     # Handle actions
     action = req["result"]["action"]
-    if action == 'hello':
+    if action == "hello":
         return respond(speech="Hi " + user.github_login)
-    if action == 'ask_for_help':
+    if action == "ask_for_help":
         # TODO get organization and topics
         topics = ["Python"]
         return respond(speech=ask_for_help(user, topics))
